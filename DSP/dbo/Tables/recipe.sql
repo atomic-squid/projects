09867f51ff2c225c537536f5aeec047511687f8c
Jason Farb <jason.farb@gmail.com>
2025-07-28 20:24:19 -0500

    Add unique constraint on recipe name column

--- DSP/dbo/Tables/recipe.sql
+++ DSP/dbo/Tables/recipe.sql
@@ -6,6 +6,7 @@ CREATE TABLE [dbo].[recipe] (
 , [cycle_time_s]  DECIMAL(4,2)     NOT NULL
 , [speedup]       BIT              NOT NULL
 , [extra_item]    BIT              NOT NULL
+, CONSTRAINT [CNS_recipe_name]         UNIQUE ([name])
 , CONSTRAINT [FK_recipe_mfg_class_id]  FOREIGN KEY (mfg_class_id)  REFERENCES mfg_class (mfg_class_id)
 , CONSTRAINT [FK_recipe_item_class_id] FOREIGN KEY (item_class_id) REFERENCES item_class (item_class_id)
 );
